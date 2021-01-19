39cfdfe6630f1fb0717365b6906795589466474b
sunny kyung <shkyung@lunit.io>
2021-01-19 23:44:24 +0900

    replace connect - state from useSelector, action function using dispatch from useDispatch

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
+let prevDispatch;
+let prevIncrease;
+let prevDecrease;
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+
+  // except dispatch, onIncrease and onDecrease without useCallback
+  // they are being created every time it is rendered
+  console.error('prevDispatch === dispatch : ', prevDispatch === dispatch);
+  console.error('prevIncrease === onIncrease : ', prevIncrease === onIncrease);
+  console.error('prevDecrease === onDecrease : ', prevDecrease === onDecrease);
+
+  prevDispatch = dispatch;
+  prevIncrease = onIncrease;
+  prevDecrease = onDecrease;
+
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
 
-export default connect(
-  (state) => {
-    return { number: state.counter.number };
-  },
-  { increase, decrease },
-)(CounterContainer);
+export default CounterContainer;
